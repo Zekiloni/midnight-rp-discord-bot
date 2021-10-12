faca1f932c126568c5ba63b38d9a97ec16197e38
P1T1X <pitixboss@users.noreply.github.com>
2021-10-13 00:04:50 +0200

    Minor Fixes

--- src/Managers/Message.Manager.ts
+++ src/Managers/Message.Manager.ts
@@ -4,11 +4,10 @@ import { Bot } from '../main';
 import { Logger, LogType } from '../Utils';
 
 const Forbiden_Words = [
-   'jebem ti'
+   'jebem ti', 'bottest'
 ]
 
-
-Bot.on('createMessage', Message => { 
+Bot.on('messageCreate', Message => {
    if (Forbiden_Words.some(word => Message.toString().toLowerCase().includes(word))) {
       Message.delete().catch((e: string) => Logger(LogType.Error, 'Forbiden Words ' + e));
    };
