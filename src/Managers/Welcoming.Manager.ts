5e8b848a4e49b370eaf6312cb6348c7276c2cdcb
Zekiloni <zekirija17@gmail.com>
2021-10-12 19:12:28 +0200

    Welcome Message

--- src/Managers/Welcoming.Manager.ts
+++ src/Managers/Welcoming.Manager.ts
@@ -1,16 +1,12 @@
 import { TextChannel } from 'discord.js';
-import { ChannelTypes } from 'discord.js/typings/enums';
 import Config from '../Config';
 import { Messages } from '../Globals/Messages';
 import { Bot } from '../main';
-import { Guild } from '../Utils';
 
 
 Bot.on('guildMemberAdd', Member => {
-   const Channel = Guild?.channels.cache.get(Config.Gateway_Channel) as TextChannel;
+   const Channel = Member.guild.channels.cache.get(Config.Gateway_Channel) as TextChannel;
    const Message = '<@' + Member.id + '> ' + Messages.WELCOME;
-   
-   if (Channel && Channel?.type == 'GUILD_TEXT') Channel.send(Message);
-   // const Channel:TextChannel = member.guild.channels.cache.get('.....');
-   // Channel?.send("Welcome"); 
+
+   if (Channel && Channel?.type == 'GUILD_TEXT') Channel.send(Message); 
 });
